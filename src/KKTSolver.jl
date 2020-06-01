4f38ce31e2991aa7f2005581518eb014e3dedc48
mtanneau <mathieu.tanneau@gmail.com>
2020-06-01 10:00:21 -0400

    Add factorization check and rename to KKT

--- src/KKTSolver.jl
+++ src/KKTSolver.jl
@@ -39,70 +39,71 @@ Tulip.KKT.backend(::UnitBlockAngularFactor) = "UnitBlockAngular"
 Tulip.KKT.linear_system(::UnitBlockAngularFactor) = "Normal equations"
 
 function Tulip.KKT.update!(
-    ls::UnitBlockAngularFactor{Tv},
+    kkt::UnitBlockAngularFactor{Tv},
     θ::AbstractVector{Tv},
     regP::AbstractVector{Tv},
     regD::AbstractVector{Tv}
 ) where{Tv<:Real}
 
-    m0 = ls.A.m0
-    n0 = ls.A.n0
-    n  = ls.A.n
-    R  = ls.A.R
+    m0 = kkt.A.m0
+    n0 = kkt.A.n0
+    n  = kkt.A.n
+    R  = kkt.A.R
 
     # Copy scaling
-    ls.θ .= θ
-    ls.regP .= regP
-    ls.regD .= regD
+    kkt.θ .= θ
+    kkt.regP .= regP
+    kkt.regD .= regD
 
     # I. Compute normal equations
     # I.1 - Schur complement `Φ = Σ Bi * Θi * Bi'`
-    θ_ = one(Tv) ./ (ls.θ .+ ls.regP)
+    θ_ = one(Tv) ./ (kkt.θ .+ kkt.regP)
 
     # Linking block
-    @views θ0 = θ_[1:(ls.A.n0)]
-    copyto!(ls._B0, ls.A.B0)
-    rmul!(ls._B0, Diagonal(sqrt.(θ0)))
-    BLAS.syrk!('U', 'N', one(Tv), ls._B0, zero(Tv), ls.C)  # C = B0 * Θ0 * B0'
+    @views θ0 = θ_[1:(kkt.A.n0)]
+    copyto!(kkt._B0, kkt.A.B0)
+    rmul!(kkt._B0, Diagonal(sqrt.(θ0)))
+    BLAS.syrk!('U', 'N', one(Tv), kkt._B0, zero(Tv), kkt.C)  # C = B0 * Θ0 * B0'
 
     # Columns block
-    @views θ1 = θ_[(1+ls.A.n0):end]
-    copyto!(ls._B, ls.A.B)
-    rmul!(ls._B , Diagonal(sqrt.(θ1)))
-    BLAS.syrk!('U', 'N', one(Tv), ls._B, one(Tv), ls.C)  # C += Σ Bi * Θi * Bi'
+    @views θ1 = θ_[(1+kkt.A.n0):end]
+    copyto!(kkt._B, kkt.A.B)
+    rmul!(kkt._B , Diagonal(sqrt.(θ1)))
+    BLAS.syrk!('U', 'N', one(Tv), kkt._B, one(Tv), kkt.C)  # C += Σ Bi * Θi * Bi'
     
     # I.2 Lower block
-    ls.L .= zero(Tv)
+    kkt.L .= zero(Tv)
     @inbounds for j in 1:n
         z = θ1[j]
-        k = ls.A.blockidx[j]
+        k = kkt.A.blockidx[j]
         @inbounds for i in 1:m0
-            ls.L[i, k] += z * ls.A.B[i, j]
+            kkt.L[i, k] += z * kkt.A.B[i, j]
         end
     end
 
     # I.2 Compute diagonal
     # Di = Σ { θj | block[j] == i }
-    ls.D .= 0
-    @inbounds for (i, θj) in zip(ls.A.blockidx, θ1)
-        ls.D[i] += θj
+    kkt.D .= 0
+    @inbounds for (i, θj) in zip(kkt.A.blockidx, θ1)
+        kkt.D[i] += θj
     end
 
     # I.3 Apply dual regularizations
-    ls.D .+= ls.regD[(ls.A.m0+1):end]
+    kkt.D .+= kkt.regD[(kkt.A.m0+1):end]
 
-    
+
     # II. Compute the factorization
-    ls._L .= ls.L
-    rmul!(ls._L, Diagonal(sqrt.(inv.(ls.D))))
-    BLAS.syrk!('U', 'N', -one(Tv), ls._L, one(Tv), ls.C)  # C -= (Di)⁻¹ (Biθi) (Biθi)'
+    kkt._L .= kkt.L
+    rmul!(kkt._L, Diagonal(sqrt.(inv.(kkt.D))))
+    BLAS.syrk!('U', 'N', -one(Tv), kkt._L, one(Tv), kkt.C)  # C -= (Di)⁻¹ (Biθi) (Biθi)'
     
     # 6. Factorize C
     # Dual regularizations on C
     @inbounds for i in 1:m0
-        ls.C[i, i] += ls.regD[i]
+        kkt.C[i, i] += kkt.regD[i]
     end
-    LAPACK.potrf!('U', ls.C)
+    _, ic = LAPACK.potrf!('U', kkt.C)
+    ic == 0 || throw(PosDefException(ic))
 
     # Done
     return nothing
@@ -110,32 +111,34 @@ end
 
 function Tulip.KKT.solve!(
     dx, dy,
-    ls::UnitBlockAngularFactor{Tv},
+    kkt::UnitBlockAngularFactor{Tv},
     ξp, ξd
 ) where{Tv<:Real}
-    m0 = ls.A.m0
-    R = ls.A.R
+    m0 = kkt.A.m0
+    R = kkt.A.R
     # TODO: dimension checks
 
     # 1. Setup right-hand side for normal equations
     dy .= ξp 
-    mul!(dy, ls.A, ξd ./ (ls.θ .+ ls.regP), one(Tv), one(Tv))
+    mul!(dy, kkt.A, ξd ./ (kkt.θ .+ kkt.regP), one(Tv), one(Tv))
     @views dy0 = dy[1:m0]
     @views dy1 = dy[(m0+1):end]
 
-    dy1 ./= ls.D
-    mul!(dy0, ls.L, dy1, -one(Tv), one(Tv))
+    dy1 ./= kkt.D
+    mul!(dy0, kkt.L, dy1, -one(Tv), one(Tv))
 
     # 2. Solve Schur complement
-    LAPACK.potrs!('U', ls.C, dy0)
+    LAPACK.potrs!('U', kkt.C, dy0)
 
     # 3. Recover solution to dy1
-    dy1 .*= ls.D
-    mul!(dy1, ls.L', dy0, -one(Tv), one(Tv))
-    dy1 ./= ls.D
+    dy1 .*= kkt.D
+    mul!(dy1, kkt.L', dy0, -one(Tv), one(Tv))
+    dy1 ./= kkt.D
 
     # 4. Recover solution to augmented system
-    dx .= (ls.A' * dy - ξd) ./ (ls.θ .+ ls.regP)
+    mul!(dx, kkt.A', dy)
+    dx .-= ξd
+    dx ./= kkt.θ .+ kkt.regP
 
     return nothing
 end
